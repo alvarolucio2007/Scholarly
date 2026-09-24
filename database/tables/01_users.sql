eea299809899ffdd416894e80085f44f4392002d
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-24 11:50:33 -0300

    docs: Removing spaces

--- database/tables/01_users.sql
+++ database/tables/01_users.sql
@@ -8,8 +8,6 @@ CREATE TABLE users(
   updated_at TIMESTAMPTZ,
 
   CONSTRAINT users_email_format CHECK (email ~* '^[^@\s]+@[^@\s]+\.[^@\s]+$'),
-
   CONSTRAINT users_name_not_empty CHECK (LENGTH(TRIM(name)) > 0),
-
   CONSTRAINT users_cpf_format CHECK (cpf IS NULL OR cpf ~ '^\d{11}$')
 )
