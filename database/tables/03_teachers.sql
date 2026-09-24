0fa6e893f6965b0f4d8b0a7e3a7e4cf8186cae1b
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-24 12:22:03 -0300

    docs: change department from unique to not null, and changing constraint name to teachers_department...

--- database/tables/03_teachers.sql
+++ database/tables/03_teachers.sql
@@ -1,6 +1,6 @@
 CREATE TABLE teachers(
   user_id BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
-  department VARCHAR(100) UNIQUE,
+  department VARCHAR(100) NOT NULL,
 
-  CONSTRAINT department_not_empty CHECK (LENGTH(TRIM(department))>0)
+  CONSTRAINT teachers_department_not_empty CHECK (LENGTH(TRIM(department))>0)
 )
