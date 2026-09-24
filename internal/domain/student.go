b5942f21cdc8603f64ed6d57389b77601b77bd01
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-24 17:24:07 -0300

    chore: Removing ErrEnrollmentNumberRequired

--- internal/domain/student.go
+++ internal/domain/student.go
@@ -1,11 +1,5 @@
 package domain
 
-import (
-	"errors"
-)
-
-var ErrEnrollmentNumberRequired = errors.New("enrollment number is required")
-
 type Student struct {
 	UserID           int64
 	EnrollmentNumber string
