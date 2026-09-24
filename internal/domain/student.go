86d677a1f266305bd703bbb60aa166eb2f3a1619
Álvaro Lúcio <alvarolucio2007@gmail.com>
2026-09-24 17:02:47 -0300

    feat: Add student domain file

--- internal/domain/student.go
+++ internal/domain/student.go
@@ -2,7 +2,6 @@ package domain
 
 import (
 	"errors"
-	"strings"
 )
 
 var ErrEnrollmentNumberRequired = errors.New("enrollment number is required")
@@ -10,11 +9,4 @@ var ErrEnrollmentNumberRequired = errors.New("enrollment number is required")
 type Student struct {
 	UserID           int64
 	EnrollmentNumber string
-}
-
-func (s *Student) Validate() error {
-	if strings.TrimSpace(s.EnrollmentNumber) == "" {
-		return ErrEnrollmentNumberRequired
-	}
-	return nil
 }
